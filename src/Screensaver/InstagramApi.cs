94c6d018bc4ad1ded99c337c40eb9b27c656b4b1
JP Toto <james.p.toto@gmail.com>
2011-12-15 21:34:53 -0500

    api work

---
+++ src/Screensaver/InstagramApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+
+namespace ScreenSaver
+{
+    class InstagramApi
+    {        
+        const string BaseUrl = "https://api.instagram.com/v1/users/self/feed";
+
+        readonly string _oauthToken;
+
+        public InstagramApi(string oauthToken)
+        {
+            _oauthToken = oauthToken;
+        }
+
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient();
+            client.BaseUrl = BaseUrl;
+            request.AddParameter("access_token", _oauthToken, ParameterType.UrlSegment); // used on every request
+            var response = client.Execute<T>(request);
+            return response.Data;
+        }
+
+        
+    }
+}
